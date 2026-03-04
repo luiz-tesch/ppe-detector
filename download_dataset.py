a86cbf14c7a3222e8673fe1eb5db1a30f46a11eb
batman <luizarthurtesch@gmail.com>
2026-03-04 13:14:43 -0300

    Update ignore, requirements + dataset download and train files

--- download_dataset.py
+++ download_dataset.py
@@ -1,5 +1,10 @@
 from dotenv import load_dotenv
+from roboflow import Roboflow
 import os
 
 load_dotenv()
-api_key = os.getenv("ROBOFLOW_API_KEY")
+
+rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
+project = rf.workspace("workspace-epi").project("hard-hat-universe-0dy7t-tbkpp")
+version = project.version(1)
+dataset = version.download("yolov8", location="dataset")
